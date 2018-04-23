3ebd2957bee1e4adf4d6cac0a121baae73e96bf7
Tin Nguyen <tinnguyen263@outlook.com>
2018-04-23 23:08:38 +0700

    add react-native-navigation lib, basic setup (android only)

--- android/app/src/main/java/com/travelapp/MainActivity.java
+++ android/app/src/main/java/com/travelapp/MainActivity.java
@@ -1,8 +1,8 @@
 package com.travelapp;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends SplashActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
