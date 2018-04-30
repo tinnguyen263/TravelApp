8b3d88fbca657e00c2ce0b059fdd01d5c1e88168
Tin Nguyen <ctinute@gmail.com>
2018-04-30 16:51:22 +0700

    add redix-logger

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -4,6 +4,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
 import initialState from "../reducers/initialState";
 
@@ -15,4 +16,4 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default initStore = () => createStore(persistedReducer, initialState, applyMiddleware(thunk));
+export default initStore = () => createStore(persistedReducer, initialState, applyMiddleware(thunk, logger));
